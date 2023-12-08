fef67e0c19a2264125faf2561d29c78f92b73c64
Anderson Martins <anderson.aguiar@outlook.com.br>
2023-12-08 14:57:08 -0300

    Done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
+import { useContext } from 'react';
 import './styles.css'
+import { ContextListCount } from '../../utils/context-listing';
 
 export default function Header() {
+    const { contextListCount } = useContext(ContextListCount);
     return (
         <header>
             <div className='dsf-container dsf-header-content'>
                 <h1>DSFilter</h1>
-                <p>6 produto(s)</p>
+                <p>{contextListCount} produto(s)</p>
             </div>
         </header>
     );
--- src/components/Listing/index.tsx
+++ src/components/Listing/index.tsx
@@ -1,20 +1,26 @@
+import { ProductDTO } from '../../models/product';
 import './styles.css';
 
-export default function Listing(){
-    return(
+type Props = {
+    products: ProductDTO[];
+}
+export default function Listing({ products }: Props) {
+    return (
         <section className='dsf-container dsf-listing-section'>
-            <div className='dsf-listing-product'>
-                <h3>PC Gamer Pro</h3>
-                <p>R$ 1200.00</p>
-            </div>
-            <div className='dsf-listing-product'>
-                <h3>PC Gamer Pro</h3>
-                <p>R$ 1200.00</p>
-            </div>
-            <div className='dsf-listing-product'>
-                <h3>PC Gamer Pro</h3>
-                <p>R$ 1200.00</p>
-            </div>
+
+            {
+                products.map(
+                    product =>
+                        <>
+                            <div className='dsf-listing-product'key={product.id}>
+                                <h3 >{product.name}</h3>
+                                <p>R$ {product.price.toFixed(2)}</p>
+                            </div>
+                        </>
+                )
+            }
+
+
         </section>
     );
 }
